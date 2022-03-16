cab862e39f9db31786d949c438fb7780d4c5f9eb
Bill Parrott <bill@chimericdream.com>
2022-03-15 22:28:27 -0500

    refactor: minor changes to work on 1.18.1

--- src/main/java/net/fabricmc/chimericdream/tag/ShelfStorageTags.java
+++ src/main/java/net/fabricmc/chimericdream/tag/ShelfStorageTags.java
@@ -1,15 +1,13 @@
 package net.fabricmc.chimericdream.tag;
 
 import net.fabricmc.chimericdream.ModInfo;
+import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
-import net.minecraft.tag.TagKey;
+import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ShelfStorageTags {
-    public static final TagKey<Block> BOOKSHELVES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves"));
+    public static final Tag<Block> BOOKSHELVES = TagFactory.BLOCK.create(new Identifier(ModInfo.MOD_ID, "bookshelves"));
 
-    public static void init() {
-
-    }
+    public static void init() {}
 }
