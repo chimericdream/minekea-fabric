04950c8422aa8e3e2ea28f806bd9da4163e3ac0f
Bill Parrott <bill@chimericdream.com>
2022-03-21 22:59:25 -0500

    feat(enchanting): make enchanting table work with new bookshelves

--- src/main/java/net/fabricmc/chimericdream/mixin/EnchantmentScreenHandlerMixin.java
+++ src/main/java/net/fabricmc/chimericdream/mixin/EnchantmentScreenHandlerMixin.java
@@ -4,20 +4,20 @@ import net.fabricmc.chimericdream.block.bookshelf.Bookshelves;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.EnchantingTableBlock;
+import net.minecraft.screen.EnchantmentScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Arrays;
 
-@Mixin(EnchantingTableBlock.class)
-public class EnchantingTableBlockMixin {
+@Mixin(EnchantmentScreenHandler.class)
+public class EnchantmentScreenHandlerMixin {
     @Redirect(
-        method = "canAccessBookshelf",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z")
+        method = "method_17411",
+        at = @At(value = "INVOKE", target = "net/minecraft/block/BlockState.isOf (Lnet/minecraft/block/Block;)Z")
     )
-    private static boolean isBookshelf(BlockState blockState, Block block) {
+    private boolean isBookshelf(BlockState blockState, Block block) {
         return blockState.getBlock().equals(Blocks.BOOKSHELF)
             || Arrays.stream(Bookshelves.getShelvesForEnchanting()).anyMatch(shelf -> blockState.getBlock().equals(shelf));
     }
