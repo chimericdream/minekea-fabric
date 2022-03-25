5e7f1d208eb60d681a3fd5ba38e8645585a7c7ba
Bill Parrott <bill@chimericdream.com>
2022-03-25 00:50:06 -0500

    feat(tags): missed adding tags to registration class

--- src/main/java/com/chimericdream/shelfstorage/tag/ShelfStorageTags.java
+++ src/main/java/com/chimericdream/shelfstorage/tag/ShelfStorageTags.java
@@ -8,6 +8,8 @@ import net.minecraft.util.registry.Registry;
 
 public class ShelfStorageTags {
     public static final TagKey<Block> BOOKSHELVES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves"));
+    public static final TagKey<Block> BOOKSHELVES_WITH_STORAGE = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves_with_storage"));
+    public static final TagKey<Block> CRATES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "crates"));
 
     public static void init() {
 
