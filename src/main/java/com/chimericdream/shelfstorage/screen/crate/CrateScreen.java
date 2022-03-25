21ccd9e8ad2613cc4c4385612f3d8901519949f6
Bill Parrott <bill@chimericdream.com>
2022-03-25 08:52:36 -0500

    feat(screens): consolidate some code

--- src/main/java/com/chimericdream/shelfstorage/screen/crate/CrateScreen.java
+++ src/main/java/com/chimericdream/shelfstorage/screen/crate/CrateScreen.java
@@ -1,37 +1,11 @@
 package com.chimericdream.shelfstorage.screen.crate;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.math.MatrixStack;
+import com.chimericdream.shelfstorage.screen.SimpleInventoryScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 
-public class CrateScreen extends HandledScreen<CrateScreenHandler> {
-    private static final Identifier TEXTURE = new Identifier("minecraft", "textures/gui/container/generic_54.png");
-
+public class CrateScreen extends SimpleInventoryScreen<CrateScreenHandler> {
     public CrateScreen(CrateScreenHandler handler, PlayerInventory inventory, Text title) {
-        super(handler, inventory, title);
-        this.backgroundHeight = 114 + 6 * 18;
-        this.playerInventoryTitleY = this.backgroundHeight - 94;
-    }
-
-    @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        this.renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
-        this.drawMouseoverTooltip(matrices, mouseX, mouseY);
-    }
-
-    @Override
-    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        int i = (this.width - this.backgroundWidth) / 2;
-        int j = (this.height - this.backgroundHeight) / 2;
-        this.drawTexture(matrices, i, j, 0, 0, this.backgroundWidth, 6 * 18 + 17);
-        this.drawTexture(matrices, i, j + 6 * 18 + 17, 0, 126, this.backgroundWidth, 96);
+        super(handler, 6, inventory, title);
     }
 }
