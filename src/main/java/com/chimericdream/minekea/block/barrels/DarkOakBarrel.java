194326e52dd0bab703f05dd023e5d3e45b8d3ab2
Bill Parrott <bill@chimericdream.com>
2022-04-07 00:30:00 -0500

    feat(arrp): move the rest of the barrel resources to code

--- src/main/java/com/chimericdream/minekea/block/barrels/DarkOakBarrel.java
+++ src/main/java/com/chimericdream/minekea/block/barrels/DarkOakBarrel.java
@@ -1,23 +1,7 @@
 package com.chimericdream.minekea.block.barrels;
 
-import com.chimericdream.minekea.ModInfo;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
 public class DarkOakBarrel extends GenericBarrel {
-    public static final Identifier BLOCK_ID = new Identifier(ModInfo.MOD_ID, "barrels/dark_oak_barrel");
-    public static final Identifier MODEL_ID = new Identifier(ModInfo.MOD_ID, "block/barrels/dark_oak_barrel");
-    public static final Identifier OPEN_MODEL_ID = new Identifier(ModInfo.MOD_ID, "block/barrels/dark_oak_barrel_open");
-    public static final String PLANK_MATERIAL = "minecraft:dark_oak_planks";
-    public static final String SLAB_MATERIAL = "minecraft:dark_oak_slab";
-
-    public void register() {
-        Registry.register(Registry.BLOCK, BLOCK_ID, this);
-        Registry.register(Registry.ITEM, BLOCK_ID, new BlockItem(this, new Item.Settings().group(ItemGroup.DECORATIONS)));
-
-        setupResources(new Identifier[]{BLOCK_ID, MODEL_ID, OPEN_MODEL_ID}, new String[]{PLANK_MATERIAL, SLAB_MATERIAL});
+    public DarkOakBarrel() {
+        super("dark_oak");
     }
 }
