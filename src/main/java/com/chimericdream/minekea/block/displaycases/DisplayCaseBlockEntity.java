a3fa3e12cc3e9e350c86d5c0b9d71d688bd7b1c2
Bill Parrott <bill@chimericdream.com>
2022-04-18 15:38:44 -0500

    feat(display cases): add sounds and named item rendering

--- src/main/java/com/chimericdream/minekea/block/displaycases/DisplayCaseBlockEntity.java
+++ src/main/java/com/chimericdream/minekea/block/displaycases/DisplayCaseBlockEntity.java
@@ -4,6 +4,7 @@ import com.chimericdream.minekea.util.ImplementedInventory;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SidedInventory;
 import net.minecraft.item.ItemStack;
@@ -11,6 +12,9 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.BlockEntityUpdateS2CPacket;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -69,5 +73,21 @@ public class DisplayCaseBlockEntity extends BlockEntity implements ImplementedIn
     @Override
     public boolean canExtract(int slot, ItemStack stack, Direction direction) {
         return false;
+    }
+
+    public void playRemoveItemSound() {
+        playSound(SoundEvents.ENTITY_ITEM_FRAME_REMOVE_ITEM);
+    }
+
+    public void playRotateItemSound() {
+        playSound(SoundEvents.ENTITY_ITEM_FRAME_ROTATE_ITEM);
+    }
+
+    public void playAddItemSound() {
+        playSound(SoundEvents.ENTITY_ITEM_FRAME_ADD_ITEM);
+    }
+
+    public void playSound(SoundEvent soundEvent) {
+        this.world.playSound((PlayerEntity) null, pos.getX(), pos.getY(), pos.getZ(), soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
     }
 }
