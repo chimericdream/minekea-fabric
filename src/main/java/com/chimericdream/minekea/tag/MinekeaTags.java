ae4a3988e50482dd8f0c913217de02a6edcacca7
Bill Parrott <bill@chimericdream.com>
2022-04-22 01:09:32 -0500

    feat(tables): connected textures/models for tables working

--- src/main/java/com/chimericdream/minekea/tag/MinekeaTags.java
+++ src/main/java/com/chimericdream/minekea/tag/MinekeaTags.java
@@ -7,11 +7,16 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class MinekeaTags {
-    public static final TagKey<Block> BOOKSHELVES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves"));
-    public static final TagKey<Block> BOOKSHELVES_WITH_STORAGE = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves_with_storage"));
-    public static final TagKey<Block> CRATES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "crates"));
+    public static final TagKey<Block> BOOKSHELVES;
+    public static final TagKey<Block> BOOKSHELVES_WITH_STORAGE;
+    public static final TagKey<Block> CRATES;
 
-    public static void init() {
+    static {
+        BOOKSHELVES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves"));
+        BOOKSHELVES_WITH_STORAGE = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves_with_storage"));
+        CRATES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "crates"));
+    }
 
+    public void init() {
     }
 }
