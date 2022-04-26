a3a2aac6f5a782fcd47fc41bdc58d21b5af5596c
Bill Parrott <bill@chimericdream.com>
2022-04-26 09:13:12 -0500

    feat(tags): some more tag cleanup and stuff

--- src/main/java/com/chimericdream/minekea/tag/MinekeaTags.java
+++ src/main/java/com/chimericdream/minekea/tag/MinekeaTags.java
@@ -1,20 +1,7 @@
 package com.chimericdream.minekea.tag;
 
-import com.chimericdream.minekea.ModInfo;
-import net.minecraft.block.Block;
-import net.minecraft.tag.TagKey;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-
 public class MinekeaTags {
-    public static final TagKey<Block> BOOKSHELVES;
-    public static final TagKey<Block> BOOKSHELVES_WITH_STORAGE;
-    public static final TagKey<Block> CRATES;
-
     static {
-        BOOKSHELVES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves"));
-        BOOKSHELVES_WITH_STORAGE = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "bookshelves_with_storage"));
-        CRATES = TagKey.of(Registry.BLOCK_KEY, new Identifier(ModInfo.MOD_ID, "crates"));
     }
 
     public void init() {
