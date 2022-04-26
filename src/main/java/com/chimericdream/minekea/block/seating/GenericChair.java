687b936ae7e5f2c7e2765cd4932be9c91da668df
Bill Parrott <bill@chimericdream.com>
2022-04-26 00:20:37 -0500

    feat(chairs): allow rotating chairs after placement

--- src/main/java/com/chimericdream/minekea/block/seating/GenericChair.java
+++ src/main/java/com/chimericdream/minekea/block/seating/GenericChair.java
@@ -1,5 +1,6 @@
 package com.chimericdream.minekea.block.seating;
 
+import com.chimericdream.minekea.MinekeaMod;
 import com.chimericdream.minekea.ModInfo;
 import com.chimericdream.minekea.resource.LootTable;
 import com.chimericdream.minekea.resource.MinekeaResourcePack;
@@ -111,9 +112,15 @@ public class GenericChair extends Block {
             return ActionResult.SUCCESS;
         }
 
+        MinekeaMod.LOGGER.info("onUse()");
+
         List<SeatEntity> seats = world.getEntitiesByClass(SeatEntity.class, new Box(pos), (Object) -> true);
 
-        if (seats.size() == 0 && player.getMainHandStack().isEmpty()) {
+        if (seats.size() == 0 && player.isSneaking()) {
+            world.setBlockState(pos, state.with(FACING, state.get(FACING).rotateYClockwise()));
+
+            return ActionResult.SUCCESS;
+        } else if (seats.size() == 0 && player.getMainHandStack().isEmpty()) {
             SeatEntity seat = Seats.SEAT_ENTITY.create(world);
             Vec3d seatPos = new Vec3d(hit.getBlockPos().getX() + 0.5d, hit.getBlockPos().getY() + 1.15d, hit.getBlockPos().getZ() + 0.5d);
 
