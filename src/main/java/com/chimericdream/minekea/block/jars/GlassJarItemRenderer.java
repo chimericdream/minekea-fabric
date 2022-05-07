7522c82368da9c850a9a3b1cebe10223593ea72b
Bill Parrott <bill@chimericdream.com>
2022-05-07 15:19:06 -0500

    feat(jars): rendering in inventory now fully works

--- src/main/java/com/chimericdream/minekea/block/jars/GlassJarItemRenderer.java
+++ src/main/java/com/chimericdream/minekea/block/jars/GlassJarItemRenderer.java
@@ -7,33 +7,35 @@ import net.fabricmc.fabric.api.renderer.v1.model.ForwardingBakedModel;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.block.BlockRenderManager;
-import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.Registry;
 
 @Environment(EnvType.CLIENT)
 public class GlassJarItemRenderer implements BuiltinItemRendererRegistry.DynamicItemRenderer {
-    private final BlockRenderManager blockRenderer = MinecraftClient.getInstance().getBlockRenderManager();
-    private final ItemRenderer renderer = MinecraftClient.getInstance().getItemRenderer();
-
     @Override
     public void render(ItemStack stack, ModelTransformation.Mode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        NbtCompound nbt = stack.getSubNbt("BlockEntityTag");
-
         BlockState defaultState = Registry.BLOCK.get(Registry.ITEM.getId(stack.getItem().asItem())).getDefaultState();
 
         JarModel model = new JarModel();
-        model.setModel(blockRenderer.getModel(defaultState));
-
+        model.setModel(MinecraftClient.getInstance().getBlockRenderManager().getModel(defaultState));
         matrices.push();
         matrices.translate(0.5, 0.5, 0.5);
-        renderer.renderItem(stack, ModelTransformation.Mode.NONE, false, matrices, vertexConsumers, light, overlay, model);
+        MinecraftClient.getInstance().getItemRenderer().renderItem(stack, ModelTransformation.Mode.NONE, false, matrices, vertexConsumers, light, overlay, model);
         matrices.pop();
+
+        GlassJarBlockEntity entity = new GlassJarBlockEntity(BlockPos.ORIGIN, defaultState);
+
+        NbtCompound nbt = stack.getSubNbt("BlockEntityTag");
+        if (nbt != null) {
+            entity.readNbt(nbt);
+        }
+
+        MinecraftClient.getInstance().getBlockEntityRenderDispatcher().renderEntity(entity, matrices, vertexConsumers, light, overlay);
     }
 
     private class JarModel extends ForwardingBakedModel {
