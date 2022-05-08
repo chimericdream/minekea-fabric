81d6929094d0c63872a86f95d11efcc807db976b
Bill Parrott <bill@chimericdream.com>
2022-05-08 14:54:15 -0500

    feat(decorations): add endless rod

--- src/main/java/com/chimericdream/minekea/block/decorations/DecorationBlocks.java
+++ src/main/java/com/chimericdream/minekea/block/decorations/DecorationBlocks.java
@@ -6,14 +6,17 @@ import com.chimericdream.minekea.util.MinekeaBlockCategory;
 import java.util.List;
 
 public class DecorationBlocks implements MinekeaBlockCategory {
+    public static final EndlessRod ENDLESS_ROD;
     public static final FakeCake FAKE_CAKE;
 
     static {
+        ENDLESS_ROD = new EndlessRod();
         FAKE_CAKE = new FakeCake();
     }
 
     @Override
     public void registerBlocks() {
+        ENDLESS_ROD.register();
         FAKE_CAKE.register();
     }
 
