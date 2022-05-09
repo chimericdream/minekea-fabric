de99d66c938f305b66a4c55364fcf72242619e64
Bill Parrott <bill@chimericdream.com>
2022-05-08 21:42:01 -0500

    feat(blocks): add basalt brick variants

--- src/main/java/com/chimericdream/minekea/block/building/BuildingBlocks.java
+++ src/main/java/com/chimericdream/minekea/block/building/BuildingBlocks.java
@@ -6,15 +6,30 @@ import com.chimericdream.minekea.util.MinekeaBlockCategory;
 import java.util.List;
 
 public class BuildingBlocks implements MinekeaBlockCategory {
-    public static final BasaltBricksBlock SMOOTH_BASALT_BRICKS_BLOCK;
+    public static final BasaltBricksBlock BASALT_BRICKS_BLOCK;
+    public static final ChiseledBasaltBricksBlock CHISELED_BASALT_BRICKS_BLOCK;
+    public static final CrackedBasaltBricksBlock CRACKED_BASALT_BRICKS_BLOCK;
+    public static final CrimsonBasaltBricksBlock CRIMSON_BASALT_BRICKS_BLOCK;
+    public static final MossyBasaltBricksBlock MOSSY_BASALT_BRICKS_BLOCK;
+    public static final WarpedBasaltBricksBlock WARPED_BASALT_BRICKS_BLOCK;
 
     static {
-        SMOOTH_BASALT_BRICKS_BLOCK = new BasaltBricksBlock();
+        BASALT_BRICKS_BLOCK = new BasaltBricksBlock();
+        CHISELED_BASALT_BRICKS_BLOCK = new ChiseledBasaltBricksBlock();
+        CRACKED_BASALT_BRICKS_BLOCK = new CrackedBasaltBricksBlock();
+        CRIMSON_BASALT_BRICKS_BLOCK = new CrimsonBasaltBricksBlock();
+        MOSSY_BASALT_BRICKS_BLOCK = new MossyBasaltBricksBlock();
+        WARPED_BASALT_BRICKS_BLOCK = new WarpedBasaltBricksBlock();
     }
 
     @Override
     public void registerBlocks() {
-        SMOOTH_BASALT_BRICKS_BLOCK.register();
+        BASALT_BRICKS_BLOCK.register();
+        CHISELED_BASALT_BRICKS_BLOCK.register();
+        CRACKED_BASALT_BRICKS_BLOCK.register();
+        CRIMSON_BASALT_BRICKS_BLOCK.register();
+        MOSSY_BASALT_BRICKS_BLOCK.register();
+        WARPED_BASALT_BRICKS_BLOCK.register();
     }
 
     @Override
