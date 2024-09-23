34d48be3029ce31f9ef59f714bb54cec9b220222
Bill Parrott <bill@chimericdream.com>
2024-09-22 23:10:05 -0500

    chore: clean up old files

--- src/main/java/com/chimericdream/minekea/resource/Recipe.java
+++
@@ -1,9 +0,0 @@
-//package com.chimericdream.minekea.resource;
-//
-//import net.minecraft.util.Identifier;
-//
-//public class Recipe {
-//    public static Identifier getRecipeID(Identifier blockID, String suffix) {
-//        return Identifier.of(blockID.getNamespace(), blockID.getPath() + suffix);
-//    }
-//}
