ecf26419a643a60745ff7d53a068d3c6488e0e4d
Bill Parrott <bill@chimericdream.com>
2024-09-29 09:49:17 -0500

    fix: nether woods shouldn't be flammable

--- src/main/java/com/chimericdream/minekea/block/furniture/displaycases/DisplayCases.java
+++ src/main/java/com/chimericdream/minekea/block/furniture/displaycases/DisplayCases.java
@@ -39,25 +39,25 @@ public class DisplayCases implements MinekeaBlockCategory {
         DISPLAY_CASES.add(new GenericDisplayCase("bamboo", "Bamboo", Blocks.BAMBOO_PLANKS, Blocks.BAMBOO_BLOCK, Blocks.STRIPPED_BAMBOO_BLOCK, true));
         DISPLAY_CASES.add(new GenericDisplayCase("birch", "Birch", Blocks.BIRCH_PLANKS, Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("cherry", "Cherry", Blocks.CHERRY_PLANKS, Blocks.CHERRY_LOG, Blocks.STRIPPED_CHERRY_LOG, true));
-        DISPLAY_CASES.add(new GenericDisplayCase("crimson", "Crimson", Blocks.CRIMSON_PLANKS, Blocks.CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM, true));
+        DISPLAY_CASES.add(new GenericDisplayCase("crimson", "Crimson", Blocks.CRIMSON_PLANKS, Blocks.CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM, false));
         DISPLAY_CASES.add(new GenericDisplayCase("dark_oak", "Dark Oak", Blocks.DARK_OAK_PLANKS, Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("jungle", "Jungle", Blocks.JUNGLE_PLANKS, Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("mangrove", "Mangrove", Blocks.MANGROVE_PLANKS, Blocks.MANGROVE_LOG, Blocks.STRIPPED_MANGROVE_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("oak", "Oak", Blocks.OAK_PLANKS, Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("spruce", "Spruce", Blocks.SPRUCE_PLANKS, Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG, true));
-        DISPLAY_CASES.add(new GenericDisplayCase("warped", "Warped", Blocks.WARPED_PLANKS, Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM, true));
+        DISPLAY_CASES.add(new GenericDisplayCase("warped", "Warped", Blocks.WARPED_PLANKS, Blocks.WARPED_STEM, Blocks.STRIPPED_WARPED_STEM, false));
 
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/acacia", "Stripped Acacia", Blocks.ACACIA_PLANKS, Blocks.STRIPPED_ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/bamboo", "Stripped Bamboo", Blocks.BAMBOO_PLANKS, Blocks.STRIPPED_BAMBOO_BLOCK, Blocks.STRIPPED_BAMBOO_BLOCK, true));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/birch", "Stripped Birch", Blocks.BIRCH_PLANKS, Blocks.STRIPPED_BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/cherry", "Stripped Cherry", Blocks.CHERRY_PLANKS, Blocks.STRIPPED_CHERRY_LOG, Blocks.STRIPPED_CHERRY_LOG, true));
-        DISPLAY_CASES.add(new GenericDisplayCase("stripped/crimson", "Stripped Crimson", Blocks.CRIMSON_PLANKS, Blocks.STRIPPED_CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM, true));
+        DISPLAY_CASES.add(new GenericDisplayCase("stripped/crimson", "Stripped Crimson", Blocks.CRIMSON_PLANKS, Blocks.STRIPPED_CRIMSON_STEM, Blocks.STRIPPED_CRIMSON_STEM, false));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/dark_oak", "Stripped Dark Oak", Blocks.DARK_OAK_PLANKS, Blocks.STRIPPED_DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/jungle", "Stripped Jungle", Blocks.JUNGLE_PLANKS, Blocks.STRIPPED_JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/mangrove", "Stripped Mangrove", Blocks.MANGROVE_PLANKS, Blocks.STRIPPED_MANGROVE_LOG, Blocks.STRIPPED_MANGROVE_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/oak", "Stripped Oak", Blocks.OAK_PLANKS, Blocks.STRIPPED_OAK_LOG, Blocks.STRIPPED_OAK_LOG, true));
         DISPLAY_CASES.add(new GenericDisplayCase("stripped/spruce", "Stripped Spruce", Blocks.SPRUCE_PLANKS, Blocks.STRIPPED_SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG, true));
-        DISPLAY_CASES.add(new GenericDisplayCase("stripped/warped", "Stripped Warped", Blocks.WARPED_PLANKS, Blocks.STRIPPED_WARPED_STEM, Blocks.STRIPPED_WARPED_STEM, true));
+        DISPLAY_CASES.add(new GenericDisplayCase("stripped/warped", "Stripped Warped", Blocks.WARPED_PLANKS, Blocks.STRIPPED_WARPED_STEM, Blocks.STRIPPED_WARPED_STEM, false));
     }
 
     @Environment(EnvType.CLIENT)
